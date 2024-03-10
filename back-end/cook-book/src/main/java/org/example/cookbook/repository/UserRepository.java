a34f632e6c3ffd2b9acb9477130d83f42757c5b9
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-03-10 22:50:10 +0200

    adding new method

--- back-end/cook-book/src/main/java/org/example/cookbook/repository/UserRepository.java
+++ back-end/cook-book/src/main/java/org/example/cookbook/repository/UserRepository.java
@@ -3,5 +3,8 @@ package org.example.cookbook.repository;
 import org.example.cookbook.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findUserByEmail(String email);
 }
