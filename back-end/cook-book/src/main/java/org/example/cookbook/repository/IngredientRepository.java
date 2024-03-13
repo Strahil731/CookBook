c79ebef1d698635a887d4e490799613f67931613
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-03-13 21:21:47 +0200

    creating repos for recipes and ingredients

---
+++ back-end/cook-book/src/main/java/org/example/cookbook/repository/IngredientRepository.java
@@ -0,0 +1,7 @@
+package org.example.cookbook.repository;
+
+import org.example.cookbook.model.entity.IngredientEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientRepository extends JpaRepository<IngredientEntity, Long> {
+}
