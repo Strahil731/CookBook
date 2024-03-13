e852c67887c42c50e40da698ad1cb5d3ba207aad
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-03-14 00:02:31 +0200

    fix

--- back-end/cook-book/src/main/java/org/example/cookbook/model/entity/BaseEntity.java
+++ back-end/cook-book/src/main/java/org/example/cookbook/model/entity/BaseEntity.java
@@ -4,8 +4,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @MappedSuperclass
+@Data
+@NoArgsConstructor
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
