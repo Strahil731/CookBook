b916bb3a1d7b7805334f8ec6343f89f5c56f7202
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-03-13 17:53:30 +0200

    update

--- front-end/src/home.js
+++
@@ -1,4 +0,0 @@
-export function showHomePage() {
-    document.querySelectorAll("section").forEach(section => section.style.display = "none");
-    document.querySelector("section.receptions").style.display = "block";
-}
