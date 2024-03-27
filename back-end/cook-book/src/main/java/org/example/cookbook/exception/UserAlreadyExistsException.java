3242bc62c842c3bccfc070a55c14c688295366c6
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-03-27 23:57:11 +0200

    removing unnecessary code

--- back-end/cook-book/src/main/java/org/example/cookbook/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.example.cookbook.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
