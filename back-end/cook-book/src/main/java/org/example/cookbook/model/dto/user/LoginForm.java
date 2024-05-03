9cca06d9ba85334ae98a66c11a02e73959fe4e56
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-03 17:51:12 +0300

    adding validation on password

--- back-end/cook-book/src/main/java/org/example/cookbook/model/dto/user/LoginForm.java
+++ back-end/cook-book/src/main/java/org/example/cookbook/model/dto/user/LoginForm.java
@@ -1,5 +1,6 @@
 package org.example.cookbook.model.dto.user;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginForm {
     private String email;
+
+    @Size(min = 3)
     private String password;
 }
--- back-end/cook-book/src/main/java/org/example/cookbook/web/rest/AuthController.java
+++ back-end/cook-book/src/main/java/org/example/cookbook/web/rest/AuthController.java
@@ -3,7 +3,10 @@ package org.example.cookbook.web.rest;
 import lombok.RequiredArgsConstructor;
 import org.example.cookbook.model.dto.user.*;
 import org.example.cookbook.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +26,11 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserDto> login(@RequestBody LoginForm loginForm) {
+    public ResponseEntity<UserDto> login(@RequestBody @Validated LoginForm loginForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
         LoginResponse response = this.userService.login(loginForm);
 
         return new ResponseEntity<>(response.user(), response.status());
